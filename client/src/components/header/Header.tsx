fe089f082ec67b636b29f34bf1520879d2babd25
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-04-04 23:14:43 +0900

    feat: Add user dropdown menu with logout using redux-persist

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,12 +1,27 @@
-import { Link } from "react-router-dom";
 import styles from "./header.module.css";
+
+import { Link, useNavigate } from "react-router-dom";
 import { useAppSelector } from "../../typedHooks/hooks";
+
 import { faAddressCard, faUserCheck } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { persistor } from "../../store";
+import { useState } from "react";
+
 const Header = () => {
   const { user } = useAppSelector((state) => state.auth);
+  const navigate = useNavigate();
 
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const handleLogout = async () => {
+    await persistor.purge();
+    navigate("/");
+  };
+
+  const handleDropDown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
   return (
     <header className={styles.header}>
       <div className={styles.container}>
@@ -14,7 +29,19 @@ const Header = () => {
           <span className={styles.logo}>SPOTON</span>
         </Link>
         {user ? (
-          user.username
+          <div className={styles.userMenu}>
+            <button onClick={handleDropDown} className={styles.userButton}>
+              {user.username}
+            </button>
+            {isDropdownOpen && (
+              <div className={styles.userDropdown}>
+                <div className={styles.logout} onClick={handleLogout}>
+                  Logout
+                </div>
+                <div className={styles.myPage}>My page</div>
+              </div>
+            )}
+          </div>
         ) : (
           <div className={styles.items}>
             <div className={styles.actionItem}>
@@ -23,7 +50,9 @@ const Header = () => {
             </div>
             <div className={styles.actionItem}>
               <FontAwesomeIcon icon={faUserCheck} size="1x" />
-              <button className={styles.button}>Login</button>
+              <Link to="/login">
+                <button className={styles.button}>Login</button>
+              </Link>
             </div>
           </div>
         )}
