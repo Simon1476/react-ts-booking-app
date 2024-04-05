9d4ba6f110ccfb3ff547312c2dc614c483ea09e5
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-04-05 20:35:46 +0900

    feat: Admin initial setup

---
+++ admin/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import LeftSideBar from "../components/LeftSideBar/LeftSideBar";
+import Navbar from "../components/Navbar/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <LeftSideBar />
+    </div>
+  );
+};
+
+export default Home;
