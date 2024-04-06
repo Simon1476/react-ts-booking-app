1d4fdbe234068a8e13434ec04c5e1e63e8908592
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-04-06 22:58:12 +0900

    [Admin]feat: Add styled-components for Home layout and widgets

--- admin/src/pages/Home.tsx
+++ admin/src/pages/Home.tsx
@@ -1,13 +1,44 @@
-import LeftSideBar from "../components/LeftSideBar/LeftSideBar";
-import Navbar from "../components/Navbar/Navbar";
+import styled from "styled-components";
+import LeftSideBar from "../components/leftSideBar/LeftSideBar";
+import Navbar from "../components/navbar/Navbar";
+import Widget from "../components/widget/Widget";
+import Featured from "../components/featured/Featured";
 
 const Home = () => {
   return (
-    <div>
-      <Navbar />
+    <S.Container>
       <LeftSideBar />
-    </div>
+      <S.DashBoardContent>
+        <Navbar />
+        <S.WidgetContainer>
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </S.WidgetContainer>
+        <S.Chart>
+          <Featured />
+        </S.Chart>
+      </S.DashBoardContent>
+    </S.Container>
   );
 };
 
 export default Home;
+
+const S = {
+  Container: styled.div`
+    display: flex;
+  `,
+  DashBoardContent: styled.div`
+    flex: 6;
+  `,
+  WidgetContainer: styled.div`
+    display: flex;
+    padding: 20px;
+    gap: 20px;
+  `,
+  Chart: styled.div`
+    padding: 5px 20px;
+  `,
+};
