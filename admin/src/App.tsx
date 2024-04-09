6e219a707ef9f39f40062068c73abaf95001d26d
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-04-09 14:42:43 +0900

    [Admin]feat: Implement Protected Routes for user authentication

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,15 +1,37 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/home/Home";
 import RootLayout from "./pages/rootLayout/RootLayout";
 import Login from "./pages/login/Login";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+
+const ProtectedRoute = () => {
+  const { state } = useContext(AuthContext);
+
+  if (!state.user) {
+    return <Navigate to="/login" />;
+  }
+
+  return <Outlet />;
+};
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
       {
-        index: true,
-        element: <Home />,
+        element: <ProtectedRoute />,
+        children: [
+          {
+            index: true,
+            element: <Home />,
+          },
+        ],
       },
     ],
   },
