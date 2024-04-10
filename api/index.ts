c01c0a50b1ec861337b61bd688894b0761f145b4
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-04-10 23:59:49 +0900

    feat: Implement CORS middleware for improved cross-origin requests

--- api/index.ts
+++ api/index.ts
@@ -6,7 +6,7 @@ import hotelsRouter from "./routes/hotels";
 import usersRouter from "./routes/users";
 import roomsRouter from "./routes/rooms";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 dotenv.config();
 const app: express.Application = express();
 
@@ -34,18 +34,19 @@ mongoose.connection.on("disconnected", () => {
 app.use(cookieParser());
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, DELETE, OPTIONS"
+//   );
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "X-Requested-With,content-type"
+//   );
+//   next();
+// });
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
