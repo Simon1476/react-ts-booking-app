c4045ffe0630df41573016d2cc803b2bf1befe18
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-04-11 00:38:53 +0900

    [Admin]feat: Expand user schema with additional user information

--- api/models/User.ts
+++ api/models/User.ts
@@ -7,6 +7,10 @@ interface DocumentResult<T> {
 interface IUser extends DocumentResult<IUser> {
   username: string;
   email: string;
+  country: string;
+  img: string;
+  city: string;
+  phone: string;
   password: string;
   isAdmin: boolean;
 }
@@ -15,6 +19,10 @@ const userSchema = new Schema<IUser>(
   {
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
+    country: { type: String, required: true },
+    img: { type: String },
+    city: { type: String, required: true },
+    phone: { type: String, required: true },
     password: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
   },
