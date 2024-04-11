22a81b973a3ca3d8e96bdf63f46c7abdf33bb5c6
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-04-11 23:54:31 +0900

    [Admin]feat: Add proxy configuration Set to http://localhost:8800

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    proxy: {
+      "/api": {
+        // 백엔드 API의 실제 URL을 지정합니다.
+        target: "http://localhost:8800",
+        // CORS 요청을 허용하도록 백엔드와 일치하도록 origin 헤더를 설정합니다.
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
--- api/index.ts
+++ api/index.ts
@@ -30,17 +30,19 @@ mongoose.connection.on("disconnected", () => {
 });
 
 // middlewares
+app.use(cors());
 
 app.use(cookieParser());
+
 app.use(express.json());
 
-app.use(cors());
 // app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
 //   res.setHeader(
 //     "Access-Control-Allow-Methods",
 //     "GET, POST, PUT, DELETE, OPTIONS"
 //   );
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
 //   res.setHeader(
 //     "Access-Control-Allow-Headers",
 //     "X-Requested-With,content-type"
